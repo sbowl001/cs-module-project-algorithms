bb374e0eee5c39d748d7fbb39c2e8e70dc473809
Stephanie <sbowl001@fiu.edu>
2020-06-12 11:32:11 +0800

    notes too

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -6,9 +6,63 @@ from collections import namedtuple
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
-    # Your code here
+    for item in items 
+      val = []
+
+      
+    Item.size
+    Item.value
 
     pass
+
+# def knapSack(W, wt, val, n):
+#    K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+#    #Table in bottom up manner
+#    for i in range(n + 1):
+#       for w in range(W + 1):
+#          if i == 0 or w == 0:
+#             K[i][w] = 0
+#          elif wt[i-1] <= w:
+#             K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]], K[i-1][w])
+#          else:
+#             K[i][w] = K[i-1][w]
+#    return K[n][W]
+# #Main
+# val = [50,100,150,200]
+# wt = [8,16,32,40]
+# W = 64
+# n = len(val)
+# print(knapSack(W, wt, val, n))
+
+# def knapSack(weight_threshold, weight_list, util_list): 
+#     """ 
+#     A Dynamic Programming algorithm that solves the 0-1 Knapsack problem.
+#     Args:
+#         weight_threshold: Weight threshold
+#         weight_list: List of weights for each item in item set I
+#         util_list: utility score of each item i
+#     Returns:
+#         The optimal utility score of the knapsack problem
+#     """
+#     n = len(util_list)
+#     lookup_table = [[0 for x in range(weight_threshold+1)] 
+#                     for x in range(n+1)] 
+  
+#     # Build table K[][] in bottom up manner 
+#     for i in range(n+1): 
+#         for w in range(weight_threshold+1): 
+#             if i==0 or w==0: 
+#                 lookup_table[i][w] = 0
+#             elif weight_list[i-1] <= w:
+#                 lookup_table[i][w] = (
+#                     max(util_list[i-1] 
+#                         + lookup_table[i-1][w-weight_list[i-1]], 
+#                         lookup_table[i-1][w]))
+#             else: 
+#                 lookup_table[i][w] = (
+#                     lookup_table[i-1][weight_threshold])
+  
+#     return lookup_table[n][weight_threshold]
 
 
 if __name__ == '__main__':
--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -23,5 +23,5 @@ def moving_zeroes(arr):
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
-    print(f'array is {arr}')
+     
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
